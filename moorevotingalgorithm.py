690950090b30a3b6de5640b451ade1a68a8ff1bd
Dineshh-007 <dineshedine007@gmail.com>
2025-06-15 12:47:22 +0530

    moorevotingalgorithm.py

--- moorevotingalgorithm.py
+++ moorevotingalgorithm.py
@@ -1,4 +1,4 @@
-#Two solutions available----->First one is otimized approach
+#Two solutions available----->First one is optimized approach
 
 #Intuition: The intuition behind this approach is that if an element occurs more than n/2 times in the array (where n is the size of the array), it will always occupy the middle position when the array is sorted. Therefore, we can sort the array and return the element at index n/2. 
 
